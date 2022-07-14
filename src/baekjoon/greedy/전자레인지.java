2db00cec7d2b14c70c3820066d04ffc95c436c32
minjee <kimmj1106@naver.com>
2022-07-14 16:04:38 +0900

    [ADD] 백준 전자레인지 풀이 추가

--- src/baekjoon/greedy/전자레인지.java
+++ src/baekjoon/greedy/전자레인지.java
@@ -11,10 +11,14 @@ public class 전자레인지 {
         int time2 = 5000;
         int time3 = 2134;
 
-        solution1(); //0 1 4
+//        solution1(); //0 1 4
         System.out.println(전자레인지.solution2(time1)); //0 1 4
         System.out.println(전자레인지.solution2(time2)); //0 1 4
         System.out.println(전자레인지.solution2(time3)); //0 1 4
+
+        System.out.println(전자레인지.solution3(time1)); //0 1 4
+        System.out.println(전자레인지.solution3(time2)); //0 1 4
+        System.out.println(전자레인지.solution3(time3)); //0 1 4
     }
 
     public static void solution1() { //백준 방식 풀이
@@ -42,7 +46,7 @@ public class 전자레인지 {
         System.out.println(time == 0 ? A + " " + B + " " + C : -1);
     }
 
-    public static String solution2(int time) { //백준 방식 풀이
+    public static String solution2(int time) {
         long startTime = System.nanoTime(); //코드 실행 시간을 알아보기 위해 추가함
 
         int aCount = 0, bCount = 0, cCount = 0; //A,B,C를 몇번씩 누를건지 count할 각각의 변수
@@ -65,5 +69,24 @@ public class 전자레인지 {
         System.out.println("seconds : " + seconds + "초");
         return time == 0 ? aCount + " " + bCount + " " + cCount : "-1"; //시간이 0이 되면 (10으로 나누어졌을 때만 0이 됨. 10으로 안나누어지면 음수가 나옴)
                                                                         // 전자레인지 a,b,c버튼 누른 순서대로 출력, 그렇지 않으면 -1 출력
+    }
+
+    public static String solution3(int time) {
+        long startTime = System.nanoTime(); //코드 실행 시간을 알아보기 위해 추가함
+
+        int[] count = {0, 0, 0}; //A,B,C를 몇번씩 누를건지 count할 배열
+        int[] fixTime = {300, 60, 10};
+
+        for (int i = 0; i < fixTime.length; i++) {
+            if (time / fixTime[i] != 0) {
+                count[i] += time / fixTime[i];
+                time %= fixTime[i];
+            }
+        }
+
+        long finishTime = System.nanoTime();
+        BigDecimal seconds = new BigDecimal((finishTime - startTime) / 100000.0);
+        System.out.println("seconds : " + seconds + "초");
+        return time == 0 ? count[0] + " " + count[1] + " " + count[2] : "-1";
     }
 }
