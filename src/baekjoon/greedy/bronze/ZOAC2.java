7214ab8ee33b83e8aabd30c4f0b49f5aa69f6108
minjee <kimmj1106@naver.com>
2022-07-18 21:05:21 +0900

    [FEAT] 백준 ZOAC2 풀이

---
+++ src/baekjoon/greedy/bronze/ZOAC2.java
@@ -0,0 +1,31 @@
+package baekjoon.greedy.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ZOAC2 {
+    public static void main(String[] args) throws IOException {
+        solution1();
+    }
+
+    public static void solution1() throws IOException { //백준 방식(BufferedReader로 한 줄 입력받기) 풀이
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String zoac = br.readLine();
+
+        int time = 0;
+        char s = 'A';
+        // 1. 아스키코드를 사용해 풀이
+        // 2. 오른쪽으로 돌리는 횟수 = abs(가야되는 위치 - 현재위치)
+        // 3. 왼쪽으로 돌리는 횟수 = 26 - 오른쪽으로 돌리는 횟수
+        // 4. 둘의 값을 비교해 최솟값을 더해주면 된다.
+
+        for(int i=0; i<zoac.length(); i++) {
+            char tmp = zoac.charAt(i);
+            int a = Math.abs(tmp - s); //오른쪽으로 돌리는 횟수
+            time += Math.min(a, 26 - a); // 오른쪽으로 돌리는 횟수 vs 왼쪽으로 돌리는 횟수
+            s = tmp;
+        }
+        System.out.println(time);
+    }
+}
