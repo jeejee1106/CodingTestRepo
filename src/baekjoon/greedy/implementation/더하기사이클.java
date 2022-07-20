133047c38faa55ed9525abab88205aa30e74de05
minjee <kimmj1106@naver.com>
2022-07-20 21:45:30 +0900

    [ADD] 백준 더하기 사이클 풀이 추가

--- src/baekjoon/greedy/implementation/더하기사이클.java
+++ src/baekjoon/greedy/implementation/더하기사이클.java
@@ -6,7 +6,13 @@ import java.io.InputStreamReader;
 
 public class 더하기사이클 {
     public static void main(String[] args) throws IOException {
-        solution1();
+//        solution1();
+
+        System.out.println(solution2(26)); //4
+        System.out.println(solution2(55)); //3
+        System.out.println(solution2(1)); //60
+        System.out.println(solution2(0)); //1
+        System.out.println(solution2(71)); //12
     }
 
     public static void solution1() throws IOException { //백준 방식(BufferedReader로 한 줄 입력받기) 풀이
@@ -24,4 +30,22 @@ public class 더하기사이클 {
 
         System.out.println(cnt);
     }
+
+    public static int solution2(int n) {
+        int answer = 0;
+        int number = n;
+
+        while (true) {
+            int left = number / 10;
+            int right = number % 10;
+            number = (right * 10 + (left + right) % 10);
+            answer++;
+            if (number == n) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+
 }
