30776de63deab3eae3f366931879019a095f88de
minjee <kimmj1106@naver.com>
2022-07-22 22:54:42 +0900

    [COMMENT] 백준 숫자의 개수 해설 추가

--- src/baekjoon/greedy/implementation/숫자의개수.java
+++ src/baekjoon/greedy/implementation/숫자의개수.java
@@ -12,18 +12,23 @@ public class 숫자의개수 {
     public static void solution1() throws IOException { //백준 방식(BufferedReader로 한 줄 입력받기) 풀이
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[10];
+        //1. 숫자 세개 입력 받아서 곱한 결과 저장
+        int number = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        
+        //2. 세 숫자의 곱을 String으로 타입 변환
+        String strNum = String.valueOf(number);
+        
+        //3. 0~9의 갯수를 저장할 배열 선언
+        int[] numArr = new int[10];
 
-        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
-
-        String str = String.valueOf(val);
-
-        for (int i = 0; i < str.length(); i++) {
-            arr[(str.charAt(i) - 48)]++;
+        //4. 배열의 인덱스에 해당하는 숫자가 있으면 count해주기
+        for (int i = 0; i < strNum.length(); i++) {
+            numArr[(strNum.charAt(i) - 48)]++;
         }
 
-        for (int v : arr) {
-            System.out.println(v);
+        //5. 출력
+        for (int count : numArr) {
+            System.out.println(count);
         }
     }
 
