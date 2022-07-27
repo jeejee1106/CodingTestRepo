8e7511ca564e66064a44047053aeff487f0ee3c2
minjee <kimmj1106@naver.com>
2022-07-27 21:43:00 +0900

    [FEAT] 백준 단어의개수 풀이

---
+++ src/baekjoon/implementation/bronze/단어의개수.java
@@ -0,0 +1,21 @@
+package baekjoon.implementation.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 단어의개수 {
+    public static void main(String[] args) throws IOException {
+        solution1();
+    }
+
+    public static void solution1() throws IOException { //백준 방식(BufferedReader로 한 줄 입력받기) 풀이
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.print(st.countTokens());
+
+    }
+
+}
