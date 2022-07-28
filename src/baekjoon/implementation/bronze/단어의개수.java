5fa59c8f6cd9f75e18933ce4a28cb685f194aae9
minjee <kimmj1106@naver.com>
2022-07-28 22:22:11 +0900

    [ADD] 백준 단어의 개수 풀이 추가

--- src/baekjoon/implementation/bronze/단어의개수.java
+++ src/baekjoon/implementation/bronze/단어의개수.java
@@ -7,7 +7,9 @@ import java.util.StringTokenizer;
 
 public class 단어의개수 {
     public static void main(String[] args) throws IOException {
-        solution1();
+//        solution1();
+        System.out.println(solution2("The Curious Case of Benjamin Button"));
+        System.out.println(solution2("Infinite KSG JDW NWH LHW LSY KMS LSJ"));
     }
 
     public static void solution1() throws IOException { //백준 방식(BufferedReader로 한 줄 입력받기) 풀이
@@ -15,7 +17,11 @@ public class 단어의개수 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         System.out.print(st.countTokens());
+    }
 
+    public static int solution2(String str) {
+        String[] strArr = str.split(" "); //split함수 : 문자열을 특정 문자를 기준으로 잘라서 배열에 넣어준다.
+        return strArr.length;
     }
 
 }
