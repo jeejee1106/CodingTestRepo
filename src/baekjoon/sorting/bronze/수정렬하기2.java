92390f23eaf2ae6a65a96bd569f428c054a2e81a
minjee <kimmj1106@naver.com>
2022-08-05 23:13:51 +0900

    [ADD] 백준 수정렬하기 - Counting Sort 직접 구현해보기

--- src/baekjoon/sorting/bronze/수정렬하기2.java
+++ src/baekjoon/sorting/bronze/수정렬하기2.java
@@ -10,7 +10,8 @@ import java.util.Collections;
 
 public class 수정렬하기2 {
     public static void main(String[] args) throws IOException {
-        solution1();
+//        solution1();
+        countingSort();
     }
 
     /**
@@ -52,7 +53,7 @@ public class 수정렬하기2 {
     public static void solution2() throws IOException {
         /**
          * 방법2 : Counting Sort(계수 정렬) 알고리즘을 응용한 방법
-         * 제일 빠른 알고리즘 solution1에 비해 약 두배 정도 빠르다.
+         * 제일 빠른(?) 알고리즘 solution1에 비해 약 두배 정도 빠르다.
          *
          * 참고 블로그 : https://st-lab.tistory.com/104
          *
@@ -63,7 +64,7 @@ public class 수정렬하기2 {
         int count = Integer.parseInt(br.readLine());
 
         /*
-		  -1000000 ~ 1000000
+		  -1,000,000 ~ 1,000,000 //문제에서의 수의 범위
 		  기준점 0 = index[100000] 으로 생각
 		*/
         boolean[] arr = new boolean[2000001];
@@ -78,5 +79,60 @@ public class 수정렬하기2 {
             }
         }
         System.out.println(sb);
+    }
+
+    public static void countingSort() {
+        /**
+         * 계수 정렬 직접 구현해보기(220805)
+         *
+         * O(n)의 시간복잡도를 가지기 때문에 속도는 분명히 빠르지만 단점도 존재한다.
+         * 아래 코드를 한 번 읽어본다면 단점을 찾을 수 있다.
+         *
+         * 바로 counting 배열이라는 새로운 배열을 선언해주어야 한다는 점이다.
+         * 생각보다 이 단점이 꽤 클 수 있는데, array 안에 있는 max값의 범위에 따라 counting 배열의 길이가 달라지게 된다.
+         * 예로들어 10개의 원소를 정렬하고자 하는데, 수의 범위가 0~1억이라면 메모리가 매우 낭비 된다.
+         * (array배열의 길이는 10인데, 각 원소의 값이 매우 크다면 counting배열의 길이는 array배열중 제일 큰 원소의 값이 길이가 된다.
+         *  즉, array = {1,2,3,4,5,6,7,8,9,1억}이라면 counting배열의 길이는 1억이 된다!)
+         */
+
+        //1. 정렬을 해야하는 배열
+        int[] array = {7, 2, 3, 5, 7, 1, 4, 6, 7, 5, 0, 1};
+
+        //2. array배열 안에 들어있는 원소의 개수를 카운팅 해주는 배열
+        int[] counting = new int[15]; //배열의 크기는 임의로 지정(간단하게 15로 지정했다. 이 배열 크기를 지정해주는 과정에서 메모리의 낭비가 많이 발생한다.)
+
+        //3. array를 한 번 순회하면서 각 값이 나올 때마다 해당 값을 index 로 하는 새로운 배열(counting)의 값을 1 증가시킨다.
+        for(int i = 0; i <array.length; i++) {
+            counting[array[i]]++;
+        }
+        //위 for문을 거치게 되면 counting[i]의 값은 array배열의 i의 개수를 나타내게 된다.
+
+        //4. counting배열의 각 값을 누적합으로 다시 넣어준다. 아래 예시처럼.
+        // counting[1] = counting[0] + counting[1]
+        // counting[2] = counting[1] + counting[2]
+        // counting[3] = counting[2] + counting[3]
+        // 식으로 나타내면 counting[i] = counting[i-1] + counting[i]
+        for(int i = 1; i<counting.length; i++) { //주의할 점은 i의 시작은 1!! index[0]은 생각하지 않는다.
+            counting[i] += counting[i-1];
+        }
+        //위 for문을 거치면 counting배열의 각 원소 값은 array배열이 정렬되어 들어갈 result배열(최종배열)의 인덱스가 된다.
+        //즉!! array배열의 각 원소값 = counting배열의 인덱스, counting배열의 원소값 - 1 = result배열의 인덱스, result배열의 원소값 = array배열의 원소값
+        //즉!! arr[i]의 값이 counting의 index값이고, counting의 값을 -1 시킨 값을 result의 index로 사용해 해당 index 값에 arr[i] 값을 넣어주게된다.
+        //이런식으로 하면 result 배열은 array 배열의 정렬된 형태로 있게 된다. 아래에서 계속 구현해보자
+
+        //5. array가 정렬된 상태로 담길 배열
+        int[] result = new int[15]; //배열의 크기 임의 지정(간단하게 15로ㅎㅎ)
+        for (int i = array.length - 1; i >= 0; i--) { //안정적으로 정렬하기 위해서는 array의 마지막 index 부터 순회하는 것이 좋다고 한다. -> 정확한 이유 알아보자
+            int value = array[i];
+            counting[value]--;
+            result[counting[value]] = value;
+        }
+
+        //6. 이제 result배열엔 array배열이 정렬되어있을 것이다. 확인해보자
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+
+
     }
 }
