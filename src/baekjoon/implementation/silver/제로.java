c29a372860ea35023c37e0662f14b865c601140f
minjee <kimmj1106@naver.com>
2022-08-10 00:11:51 +0900

    [FEAT ~ing] 백준 제로 풀이 중

---
+++ src/baekjoon/implementation/silver/제로.java
@@ -0,0 +1,28 @@
+package baekjoon.implementation.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 제로 {
+    public static void main(String[] args) throws IOException {
+        solution1();
+    }
+
+    public static void solution1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int[] arr = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+            if (arr[i] == 0) {
+                sum -=arr[i-1];
+            }
+        }
+        System.out.println(sum);
+    }
+
+}
