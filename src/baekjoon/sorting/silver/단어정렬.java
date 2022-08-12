38523eb9c565331cffc8e86ebf3664b66f3d0cf1
minjee <kimmj1106@naver.com>
2022-08-12 23:22:31 +0900

    [FEAT~ing] 백준 단어 정렬 풀이 중

---
+++ src/baekjoon/sorting/silver/단어정렬.java
@@ -0,0 +1,48 @@
+package baekjoon.sorting.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 단어정렬 {
+    public static void main(String[] args) throws IOException {
+        solution1();
+    }
+
+    public static void solution1() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+        br.close();
+
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                // 단어 길이가 같을 경우
+                if (s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                }
+                // 그 외의 경우
+                else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        sb.append(arr[0]).append('\n');
+
+        for (int i = 1; i < n; i++) {
+            // 중복되지 않는 단어만 출력
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
