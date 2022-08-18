3b044fd070467d5a476528091ce3288c76112e4d
minjee <kimmj1106@naver.com>
2022-08-19 00:00:40 +0900

    [ADD] 백준 피보나치수5 풀이 - 재귀함수 사용

--- src/baekjoon/implementation/bronze/피보나치수5.java
+++ src/baekjoon/implementation/bronze/피보나치수5.java
@@ -7,13 +7,18 @@ import java.util.ArrayList;
 
 public class 피보나치수5 {
     public static void main(String[] args) throws IOException {
-        solution1();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        solution1(n); //속도 : 124ms, 메모리 : 14112KB
+        System.out.println(fibonacciSolution(n)); //속도 : 124ms, 메모리 : 14216KB
     }
 
-    public static void solution1() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+    public static void solution1(int n) throws IOException {
+        /**
+         * 피보나치수열은 재귀함수의 예로 많이 쓰인다.
+         * 이 문제도 재귀함수로 풀 수 있으나 solution1은 ArrayList와 for문을 사용하여 풀어보았다.
+         */
         ArrayList<Integer> list = new ArrayList<>();
         list.add(0);
         list.add(1);
@@ -23,6 +28,16 @@ public class 피보나치수5 {
 
         System.out.println(list.get(n));
 
+    }
+
+    public static int fibonacciSolution(int n) throws IOException {
+        /**
+         * 재귀함수를 사용한 피보나치 수열 구현
+         */
+        if(n<=1) {
+            return n;
+        }
+        return fibonacciSolution(n-2) + fibonacciSolution(n-1);
     }
 
 }
