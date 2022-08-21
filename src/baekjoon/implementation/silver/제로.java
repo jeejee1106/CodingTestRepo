2c4880f1701218bbb996264e8a594628e9187bbe
minjee <kimmj1106@naver.com>
2022-08-21 22:11:20 +0900

    [FEAT] 백준 제로 풀이

--- src/baekjoon/implementation/silver/제로.java
+++ src/baekjoon/implementation/silver/제로.java
@@ -3,6 +3,7 @@ package baekjoon.implementation.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class 제로 {
     public static void main(String[] args) throws IOException {
@@ -11,18 +12,22 @@ public class 제로 {
 
     public static void solution1() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine()); //몇 개의 정수를 받을 것인가
         int sum = 0;
-        int[] arr = new int[k];
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-            if (arr[i] == 0) {
-                sum -=arr[i-1];
+            int n = Integer.parseInt(br.readLine());
+            if (n != 0) { //들어온 정수가 0이 아니면
+                list.add(n); //리스트에 추가하고
+            } else { //들어온 정수가 0이라면
+                list.remove(list.size() - 1); //제일 마지막 index에 해당하는 값 삭제
             }
         }
+        for (int i : list) {
+            sum += i; //list에 들어온 수의 합
+        }
+
         System.out.println(sum);
     }
-
 }
