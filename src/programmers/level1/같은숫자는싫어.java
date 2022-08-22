5f0ef1c721ae4fe44ca69d88d3873f687b3f9892
minjee <kimmj1106@naver.com>
2022-08-23 00:42:43 +0900

    [FEAT] 프로그래머스 같은 숫자는 싫어 풀이

---
+++ src/programmers/level1/같은숫자는싫어.java
@@ -0,0 +1,32 @@
+package programmers.level1;
+
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+    public static void main(String[] args) {
+        int[] a = {1,1,3,3,0,1,1};
+        int[] b = {4,4,4,3,3};
+
+        System.out.println(같은숫자는싫어.solution1(a)); //[1,3,0,1]
+        System.out.println(같은숫자는싫어.solution1(b)); //[4,3]
+    }
+
+    /////////////////[solution 시작]/////////////////
+    public static int[] solution1(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for(int i = 1; i < arr.length; i++){
+            if (arr[i] != arr[i - 1]) {
+                stack.push(arr[i]);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        for (int i = 0; i < stack.size(); i++) {
+            answer[i] = stack.get(i);
+        }
+
+        return answer;
+    }
+}
