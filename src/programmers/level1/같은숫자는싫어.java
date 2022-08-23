e0dc8bf75098396b7bb0ffe656ec08806b7f9aa9
minjee <kimmj1106@naver.com>
2022-08-24 00:21:31 +0900

    [ADD] 프로그래머스 같은 숫자는 싫어 풀이 추가

--- src/programmers/level1/같은숫자는싫어.java
+++ src/programmers/level1/같은숫자는싫어.java
@@ -1,5 +1,6 @@
 package programmers.level1;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class 같은숫자는싫어 {
@@ -25,6 +26,24 @@ public class 같은숫자는싫어 {
         int[] answer = new int[stack.size()];
         for (int i = 0; i < stack.size(); i++) {
             answer[i] = stack.get(i);
+        }
+
+        return answer;
+    }
+
+    public static int[] solution2(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+
+        for(int i = 1; i < arr.length; i++){
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
         return answer;
