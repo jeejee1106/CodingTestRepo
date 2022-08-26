075c943b27e064966bb513a874135e855791803c
minjee <kimmj1106@naver.com>
2022-08-27 01:43:31 +0900

    [FEAT] 프로그래머스 신고 결과 받기 풀이 한창 취업준비를 했을 땐 너무 어려웠는데... 다른 사람의 풀이를 안보고 풀어서 너무 기쁘다ㅎㅎ 그동안 공부한 보람이 있다ㅎㅎㅎㅎ

--- src/programmers/level1/신고결과받기.java
+++ src/programmers/level1/신고결과받기.java
@@ -3,6 +3,7 @@ package programmers.level1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class 신고결과받기 {
     /**
@@ -29,18 +30,26 @@ public class 신고결과받기 {
         //k번 이상 신고 당하면 신고한 사람들에게 메일 발송
         //각 유저별로 메일을 받은 횟수를 리턴해라
 
-        //map에 신고받은 id(key)들과 횟수(value)를 넣는다.
+        //1. 신고 중복 제거
+        HashSet<String> set = new HashSet<>();
+        for (String s : report) {
+            set.add(s);
+        }
+
+        //2. 일단 map에 유저들의 아이디와 신고받은횟수를 0으로 초기화해서 넣어준다.
         HashMap<String, Integer> reportMap = new HashMap<>();
         for (int i = 0; i < idList.length; i++) {
             reportMap.put(idList[i], 0);
         }
-        for (String s : report) { // report배열의 원소를 하나씩 s에 넣는다.
-            String[] reports = s.split(" "); //공백을 기준으로 문자열을 분리하여 배열에 다시 넣는다.
-            String badId = reports[1]; //인덱스가 1인(즉, 신고당한 id) 데이터를 가져온다.
+
+        //3. map에 신고받은 id(key)들과 횟수(value)를 넣는다.
+        for (String s : set) { // report배열의 원소를 하나씩 s에 넣는다.
+            String[] reportSplit = s.split(" "); //공백을 기준으로 문자열을 분리하여 배열에 다시 넣는다.
+            String badId = reportSplit[1]; //인덱스가 1인(즉, 신고당한 id) 데이터를 가져온다.
             reportMap.put(badId, reportMap.get(badId) + 1); //신고당한 id를 map에 넣어준다.
         }
 
-        //list에 k번 이상 신고 당한 아이디를 넣는다.
+        //4. list에 k번 이상 신고 당한 아이디를 넣는다.
         ArrayList<String> badIdList = new ArrayList<>();
         for (int i = 0; i < idList.length; i++) {
             if (reportMap.get(idList[i]) >= k) { //만약 신고당한 횟수가 k번 이상이면
@@ -48,11 +57,14 @@ public class 신고결과받기 {
             }
         }
 
+        //5. 정답을 넣을 배열 선언
         int[] answer = new int[idList.length];
-        for (String s : report) {
-            String[] reports = s.split(" ");
-            String id = reports[0];
-            String badId = reports[1];
+
+        //6.
+        for (String s : set) {
+            String[] reportSplit = s.split(" ");
+            String id = reportSplit[0]; //신고한 유저 id
+            String badId = reportSplit[1]; //신고당한 유저 id
             for (int i = 0; i < badIdList.size(); i++) {
                 if (badId.equals(badIdList.get(i))) {
                     int x = Arrays.asList(idList).indexOf(id);
