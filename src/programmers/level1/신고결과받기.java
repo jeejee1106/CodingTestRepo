f9211840575f764471515662aac26c3f6da48617
minjee <kimmj1106@naver.com>
2022-08-28 18:05:29 +0900

    [ADD] 프로그래머스 신고 결과 받기 풀이 추가

--- src/programmers/level1/신고결과받기.java
+++ src/programmers/level1/신고결과받기.java
@@ -6,11 +6,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class 신고결과받기 {
-    /**
-     * 으어ㅓ어ㅓ어어 거의 다 했는데 여러번 신고해도 한 번만 들어간다는 걸 간과함
-     * 그것만 고치면 될 듯!!!!!
-     * @param args
-     */
     public static void main(String[] args) {
         String[] idList1 = {"muzi", "frodo", "apeach", "neo"};
         String[] report1 = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
@@ -20,8 +15,11 @@ public class 신고결과받기 {
         String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
         int k2 = 3;
 
-        System.out.println(신고결과받기.solution1(idList1, report1, k1)); //[2,1,1,0]
-        System.out.println(신고결과받기.solution1(idList2, report2, k2)); //[0,0]
+//        System.out.println(신고결과받기.solution1(idList1, report1, k1)); //[2,1,1,0]
+//        System.out.println(신고결과받기.solution1(idList2, report2, k2)); //[0,0]
+
+        System.out.println(신고결과받기.solution2(idList1, report1, k1)); //[2,1,1,0]
+        System.out.println(신고결과받기.solution2(idList2, report2, k2)); //[0,0]
     }
 
     /////////////////[solution 시작]/////////////////
@@ -60,9 +58,48 @@ public class 신고결과받기 {
         //5. 정답을 넣을 배열 선언
         int[] answer = new int[idList.length];
 
-        //6.
+        //6. k번 이상 신고당한 유저를 신고한 유저에 대한 카운트++
         for (String s : set) {
             String[] reportSplit = s.split(" ");
+            String id = reportSplit[0]; //신고한 유저 id
+            String badId = reportSplit[1]; //신고당한 유저 id
+            for (int i = 0; i < badIdList.size(); i++) {
+                if (badId.equals(badIdList.get(i))) {
+                    int x = Arrays.asList(idList).indexOf(id);
+                    answer[x]++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static int[] solution2(String[] idList, String[] report, int k) {
+
+        //1. 신고 중복 제거
+        HashSet<String> reportSet = new HashSet<>(Arrays.asList(report)); //일반 배열을 ArrayList로 변환 후 set에 넣어주기
+
+        //2. map에 신고당한 유저의 id와 횟수를 넣는다.
+        HashMap<String, Integer> badUserMap = new HashMap<>();
+        for (String reportStr : reportSet) {
+            String badId = reportStr.split(" ")[1]; //신고당한 유저 id
+            badUserMap.put(badId, badUserMap.getOrDefault(badId, 0) + 1);
+        }
+
+        //3. list에 k번 이상 신고 당한 아이디를 넣는다.
+        ArrayList<String> badIdList = new ArrayList<>();
+        for (String badId : badUserMap.keySet()) {
+            if (badUserMap.get(badId) >= k) { //만약 신고당한 횟수가 k번 이상이면
+                badIdList.add(badId); //list에 해당 아이디 추가
+            }
+        }
+
+        //4. 정답을 넣을 배열 선언
+        int[] answer = new int[idList.length];
+
+        //5.
+        for (String reportStr : reportSet) {
+            String[] reportSplit = reportStr.split(" ");
             String id = reportSplit[0]; //신고한 유저 id
             String badId = reportSplit[1]; //신고당한 유저 id
             for (int i = 0; i < badIdList.size(); i++) {
