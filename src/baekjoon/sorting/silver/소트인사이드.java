0ea39500de1e4ada778e09f3ea1819b1420a7ded
minjee <kimmj1106@naver.com>
2022-09-09 22:37:52 +0900

    [ADD] 백준 소트인사이드 문제 추가

---
+++ src/baekjoon/sorting/silver/소트인사이드.java
@@ -0,0 +1,18 @@
+package baekjoon.sorting.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 소트인사이드 {
+    public static void main(String[] args) throws IOException{
+        solution1();
+    }
+
+    public static void solution1() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
