4c43a8e5c9a60aa4266e12af9baa8d3fd38c6deb
minjee <kimmj1106@naver.com>
2022-09-11 23:31:34 +0900

    [ADD] 백준 막대기 문제 추가

---
+++ src/baekjoon/stack/bronze/막대기.java
@@ -0,0 +1,20 @@
+package baekjoon.stack.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ¸·´ë±â {
+    public static void main(String[] args) throws IOException{
+        solution1();
+    }
+
+    public static void solution1() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String number = br.readLine();
+
+
+    }
+
+}
