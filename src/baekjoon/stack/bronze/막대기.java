01fdc0c569b49a015c2b385439e19f4d2756bf28
minjee <kimmj1106@naver.com>
2022-09-12 21:56:19 +0900

    [FEAT ~ing] 백준 막대기 푸는 중

--- src/baekjoon/stack/bronze/막대기.java
+++ src/baekjoon/stack/bronze/막대기.java
@@ -12,9 +12,15 @@ public class ¸·´ë±â {
     public static void solution1() throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String number = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-
+        for (int i = arr.length; i >= 0; i--) {
+            
+        }
     }
 
 }
