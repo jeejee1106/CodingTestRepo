5aa2b9701ca5b51c7dd9b95e8c008321923e0ef4
minjee <kimmj1106@naver.com>
2022-09-14 22:52:24 +0900

    [ADD] 백준 요세푸스문제0 추가

---
+++ src/baekjoon/implementation/silver/요세푸스문제0.java
@@ -0,0 +1,16 @@
+package baekjoon.implementation.silver;
+
+import java.io.IOException;
+
+public class 요세푸스문제0 {
+
+    public static void main(String[] args) throws IOException {
+          solution1();
+
+    }
+
+    public static void solution1() throws IOException {
+
+
+    }
+}
