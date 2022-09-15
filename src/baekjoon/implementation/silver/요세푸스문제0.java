d0f845ed48810e8b40b480e1f174adfdc65cefd4
minjee <kimmj1106@naver.com>
2022-09-15 23:15:29 +0900

    [FEAT~ing] 백준 요세푸스문제0 풀이 중

--- src/baekjoon/implementation/silver/요세푸스문제0.java
+++ src/baekjoon/implementation/silver/요세푸스문제0.java
@@ -1,6 +1,9 @@
 package baekjoon.implementation.silver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 요세푸스문제0 {
 
@@ -10,7 +13,11 @@ public class 요세푸스문제0 {
     }
 
     public static void solution1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
     }
 }
