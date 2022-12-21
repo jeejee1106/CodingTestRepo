89c7c0cf797a90e9b98b27da7f3c2f98d29b9019
minjee <kimmj1106@naver.com>
2022-12-21 23:22:22 +0900

    [programmers]LEVEL0 - 평행

--- src/programmers/level0/평행.java
+++ src/programmers/level0/평행.java
@@ -1,5 +1,8 @@
 package programmers.level0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 평행 {
     public static void main(String[] args) {
         int[][] dots1 = {
@@ -11,19 +14,42 @@ public class 평행 {
         int[][] dots2 = {
                 {3, 5},  //[(0,0) (0,1)]
                 {4, 1},  //[(1,0) (1,1)]
-                {2, 4},  //[(1,0) (1,1)]
+                {2, 4},  //[(2,0) (2,1)]
                 {5, 10}}; //[(3,0) (3,1)]
 
         System.out.println(평행.solution1(dots1)); //1
-        System.out.println(평행.solution1(dots2)); //2
+        System.out.println(평행.solution1(dots2)); //0
         System.out.println();
         System.out.println(평행.solution2(dots1)); //1
         System.out.println(평행.solution2(dots2)); //0
     }
+        //두 선분이 평행하려면? 기울기가 같아야한다.
+        //기울기 구하는 공식 : y값 증가량 / x값 증가량 = (y2 - y1) / (x2 - x1)
+//        (0,1 - 1,1) / (0,0 - 1,0);
+//        (0,1 - 2,1) / (0,0 - 2,0);
+//        (0,1 - 3,1) / (0,0 - 3,0);
+//
+//        (1,1 - 2,1) / (1,0 - 2,0);
+//        (1,1 - 3,1) / (1,0 - 3,0)
 
     /////////////////[solution 시작]/////////////////
     public static int solution1(int[][] dots) {
         int answer = 0;
+        List<Double> slopeList = new ArrayList<>();
+
+        for (int i = 0; i < dots.length; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                //1. 각 좌표값들의 기울기를 모두 계산해준다.
+                double slope = (double) (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
+                //2. 기울기리스트에 같은 기울기가 있으면 퍙행하는 선이 있으므로 1리턴.
+                if (slopeList.contains(slope)) {
+                    return 1;
+                }
+                //3. 기울기 리스트에 같은 기울기가 없으면 리스트에 새로 넣어주기
+                slopeList.add(slope);
+            }
+
+        }
         return answer;
     }
 
