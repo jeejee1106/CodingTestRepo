6636006d66f418dfab58a90ef9b23a7941d1e10b
minjee <kimmj1106@naver.com>
2023-02-11 12:54:44 +0900

    [programmers] LEVEL0 - 캐릭터의 좌표 풀이 추가

--- src/programmers/level0/캐릭터의좌표.java
+++ src/programmers/level0/캐릭터의좌표.java
@@ -10,9 +10,6 @@ public class 캐릭터의좌표 {
         String[] keyinput2 = {"down", "down", "down", "down", "down"};
         int[] board2 = {7, 9};
 
-        String[] keyinput3 = {"down", "down", "down", "down", "down"};
-        int[] board3 = {7, 9};
-
         System.out.println(Arrays.toString(캐릭터의좌표.solution1(keyinput1, board1))); //[2, 1]
         System.out.println(Arrays.toString(캐릭터의좌표.solution1(keyinput2, board2))); //[0, -4]
     }
@@ -25,32 +22,54 @@ public class 캐릭터의좌표 {
         int[] maxPosition = {board[0]/2, board[1]/2};
         int[] answer = {0, 0};
 
+        //switch문 풀이 속도는 아~~주 약간 더 빠른듯?
         for (int i = 0; i < keyinput.length; i++) {
-            if (keyinput[i].equals("up")) {
-                f1(answer, x, 0, y);
-                extracted(answer, maxPosition, x, 0, y);
-            } else if (keyinput[i].equals("down")) {
-                f1(answer, x, 1, y);
-                extracted(answer, maxPosition, x, 1, y);
-            } else if (keyinput[i].equals("left")) {
-                f1(answer, x, 2, y);
-                extracted(answer, maxPosition, x, 2, y);
-            } else if (keyinput[i].equals("right")) {
-                f1(answer, x, 3, y);
-                extracted(answer, maxPosition, x, 3, y);
+            switch (keyinput[i]) {
+                case "up":
+                    move(answer, x, 0, y);
+                    positionCheck(answer, maxPosition, x, 0, y);
+                    break;
+                case "down":
+                    move(answer, x, 1, y);
+                    positionCheck(answer, maxPosition, x, 1, y);
+                    break;
+                case "left":
+                    move(answer, x, 2, y);
+                    positionCheck(answer, maxPosition, x, 2, y);
+                    break;
+                case "right":
+                    move(answer, x, 3, y);
+                    positionCheck(answer, maxPosition, x, 3, y);
             }
         }
+
+        //if문 풀이
+//        for (int i = 0; i < keyinput.length; i++) {
+//            if (keyinput[i].equals("up")) {
+//                move(answer, x, 0, y);
+//                positionCheck(answer, maxPosition, x, 0, y);
+//            } else if (keyinput[i].equals("down")) {
+//                move(answer, x, 1, y);
+//                positionCheck(answer, maxPosition, x, 1, y);
+//            } else if (keyinput[i].equals("left")) {
+//                move(answer, x, 2, y);
+//                positionCheck(answer, maxPosition, x, 2, y);
+//            } else if (keyinput[i].equals("right")) {
+//                move(answer, x, 3, y);
+//                positionCheck(answer, maxPosition, x, 3, y);
+//            }
+//        }
 
 
         return answer;
     }
 
-    private static void f1(int[] answer, int[] x, int x1, int[] y) {
+    private static void move(int[] answer, int[] x, int x1, int[] y) {
         answer[0] += x[x1];
         answer[1] += y[x1];
     }
 
-    private static void extracted(int[] answer, int[] maxPosition, int[] x, int x1, int[] y) {
+    private static void positionCheck(int[] answer, int[] maxPosition, int[] x, int x1, int[] y) {
         if (Math.abs(answer[0]) > maxPosition[0]) {
             answer[0] -= x[x1];
         }
