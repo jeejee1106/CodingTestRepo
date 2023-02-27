082323bf3497061c06916b6dfcece75bfd4946af
minjee <kimmj1106@naver.com>
2023-02-28 00:20:58 +0900

    [programmers]LEVEL0 - 직사각형 넓이 구하기 수정 ㅜㅜ 같은코드 두 번 씀...

--- src/programmers/level0/직사각형넓이구하기.java
+++ src/programmers/level0/직사각형넓이구하기.java
@@ -52,39 +52,4 @@ public class 직사각형넓이구하기 {
 
         return answer;
     }
-
-    public static int solution2(int[][] dots) {
-        int answer = 0;
-        int[] xArr = new int[4];
-        int[] yArr = new int[4];
-        int x = 0;
-        int y = 0;
-
-        for (int i = 0; i < dots.length; i++) {
-            xArr[i] = dots[i][0];
-            yArr[i] = dots[i][1];
-        }
-
-        for (int i = 0; i < dots.length - 1; i++) {
-            int tmpX = xArr[i] - xArr[i + 1];
-            int tmpY = yArr[i] - yArr[i + 1];
-
-            if (tmpX != 0) {
-                x = tmpX;
-            }
-
-            if (tmpY != 0) {
-                y = tmpY;
-            }
-
-            if (x != 0 && y != 0) {
-                break;
-            }
-        }
-
-        answer = Math.abs(x * y);
-
-        return answer;
-    }
-
 }
